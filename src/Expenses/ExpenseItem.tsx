55e85059319c5a2d9caac770c2be8539da7d3bbe
Josef Gargulák <pepikgargulak@gmail.com>
2021-05-29 20:27:00 +0200

    application basis

---
+++ src/Expenses/ExpenseItem.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {ExpenseDate} from './ExpenseDate';
+import {useState} from 'react';
+import {Card} from '../UI/Card';
+
+export const ExpenseItem = (props: any) => {
+
+  const [title, setTitle] = useState(props.title);
+
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
+
+  return (<>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change title</button>
+    </Card>
+  </>)
+}
